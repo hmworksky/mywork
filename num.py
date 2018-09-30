5c99ec7ae144b6a38faa0903c08c172b4f9c494b
unknown <huangmin@wanlitong.com>
2018-09-30 11:13:01 +0800

    '1538277181.4924283'

--- num.py
+++ num.py
@@ -1,21 +1,60 @@
 
+# def num(data):
+#     data = sorted(data)
+#     length = len(data) /2
+#     list1 = []
+#     list2 = []
+#     while data:
+#         if not list1:
+#             list1.append(data.pop())
+#         if sum(list1) > sum(list2) and len(list2) < length :
+#             list2.append(data.pop())
+#         else:
+#             if len(list1) < length :
+#                 if min(data)<0:
+#                     list1.append(data.pop(0))
+#                 else:
+#                     list1.append(data.pop())
+#             else:
+#                 list2.append(data.pop())
+#     print('list1:',list1)
+#     print('list2:', list2)
+#     print('sum:', sum(list1)-sum(list2))
+
+
+
+
 def num(data):
     data = sorted(data)
+    print(data)
     length = len(data) /2
     list1 = []
     list2 = []
     while data:
-        if sum(list1) > sum(list2) and len(list2) < length:
-            list2.append(data.pop())
-        else:
-            if len(list1) < length:
-                list1.append(data.pop())
+
+        if not list1:
+            list1.append(data.pop())
+        index = 0 if min(data)<0 else -1
+        if sum(list1) > sum(list2) and len(list1) <length :
+            if min(data)<0 and abs(min(data)) >max(data):
+                list1.append(data.pop(0))
             else:
                 list2.append(data.pop())
-
+        else:
+            if len(list2) < length:
+                if min(data) < 0 and abs(min(data)) > max(data):
+                    list2.append(data.pop(0))
+                else:
+                    list1.append(data.pop())
+            else:
+                list1.append(data.pop())
+    print('list1:',list1)
+    print('list2:', list2)
+    print('sum:', sum(list1)-sum(list2))
 
 if __name__ == '__main__':
     a = [1,2,3,4,5,100]
     b = [1,2,3,4,11,12,13,14]
-    print(sorted(a))
-    num(b)
+    c = [-5,-9,0,1,5,9]
+    d = [-1,-2,-3,-4,-5,-6]
+    num(c)
