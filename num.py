f1ad1ad67f725466cb7c6d5a900e7c8f122be398
unknown <huangmin@wanlitong.com>
2018-09-30 13:13:02 +0800

    '1538284381.913179'

--- num.py
+++ num.py
@@ -1,36 +1,71 @@
-
-def num(data):
+import time
+def num(list1,list2):
+    start = time.time()
+    #2个列表合并
+    data = list1 + list2
+    #先排序一次
     data = sorted(data)
+    #获取每个列表需要填充的长度
     length = len(data) /2
     list1 = []
     list2 = []
+    #先取一个最大的
+    if not list1:
+        list1.append(data.pop())
+
+    #看看有没有小于0 而且绝对值大于差值的，如果有则加到大的数据列表
+    index =  min(data) <0 and abs(min(data)) >= abs(sum(list1) - sum(list2))
     while data:
-        if not list1:
-            list1.append(data.pop())
-        if sum(list1) > sum(list2) and len(list2) < length :
-            list2.append(data.pop())
-        else:
-            if len(list1) < length :
-                if min(data)<0:
-                    list1.append(data.pop(0))
-                else:
-                    list1.append(data.pop())
+        #判断2个列表大小，第一次进来list1肯定大于list2，所以看看差值的判断，加上长度的校验
+        if sum(list1) > sum(list2)  and len(list1) < length:
+            if index:
+                list1.append(data.pop(0))
             else:
-                list2.append(data.pop())
+                if len(list2) == length:
+                    list1.append(data.pop())
+                else:
+                    list2.append(data.pop())
+        else:
+            if len(list2) < length:
+                #添加第二个列表也需要进行判断index也就是有没有小于0而且绝对值大于差值的
+                if index:
+                    list2.append(data.pop(0))
+                else:
+                    if len(list1) == length:
+                        list2.append(data.pop())
+                    else:
+                        list1.append(data.pop())
+            #列表2如果满了，则向列表1中添加数据
+            else:
+                list1.append(data.pop())
+
     print('list1:',list1)
     print('list2:', list2)
     print('sum:', sum(list1)-sum(list2))
-
-
-
-
+    print('time:',time.time()-start)
+    return
 
 if __name__ == '__main__':
-    a = [1,2,3,4,5,100]
-    b = [1,2,3,4,11,12,13,14]
-    c = [-5,-9,0,1,5,9]
-    d = [-1,-2,-3,-4,-5,-6]
+    case_1 = list(range(10,10000))
+    case_11 = list(range(1000,30000))
+    num(case_1,case_11)
     # datas = a.pop(-1)
     # print(datas)
-    num(c)
+    list1 = [11,12,13]
+    list2 = [1,2,3]
+    num(list1,list2)
+
+    case_2 = [-1,-2,-3]
+    case_22 = [-4, -5, -6]
+    num(case_2,case_22)
+
+    case_3 = [-9, -5, -1]
+    case_33 = [9, 5, 0]
+    num(case_3, case_33)
+
+    case_4 = [1, 2, 3]
+    case_44 = [4, 5, 100]
+    num(case_4, case_44)
+
+
 
